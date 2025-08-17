68782adc919646728ea3dc4903d150729bbdc983
Jason Summer <jason.summer@snowflake.com>
2025-08-17 16:06:36 -0500

    Add object list tool

--- mcp_server_snowflake/object_manager/objects.py
+++ mcp_server_snowflake/object_manager/objects.py
@@ -4,10 +4,15 @@ from pydantic import BaseModel, Field
 from snowflake.core import Root
 from snowflake.core.database import Database
 from snowflake.core.schema import Schema
+from snowflake.core.table import Table
+from snowflake.core.warehouse import Warehouse
 
 
 class ObjectMetadata(BaseModel):
-    name: str = Field(description="The name of the object")
+    name: str | None = Field(
+        default=None,  # We don't use name when listing objects
+        description="The name of the object",
+    )
     comment: str | None = Field(
         default=None, description="The description of the object"
     )
@@ -22,11 +27,12 @@ class SnowflakeDatabase(ObjectMetadata):
         return Database.from_dict(self.__dict__)
 
     def get_core_path(self, root: Root):
-        core_object = self.get_core_object()
-        if isinstance(core_object, Database):
-            return root.databases
-        else:
-            raise ValueError(f"Invalid object type: {self}")
+        # core_object = self.get_core_object()
+        # if isinstance(core_object, Database):
+        #     return root.databases
+        # else:
+        #     raise ValueError(f"Invalid object type: {self}")
+        return root.databases
 
 
 class SnowflakeSchema(ObjectMetadata):
@@ -39,15 +45,82 @@ class SnowflakeSchema(ObjectMetadata):
         return Schema.from_dict(self.__dict__)
 
     def get_core_path(self, root: Root):
-        core_object = self.get_core_object()
-        if isinstance(core_object, Schema):
-            return root.databases[self.database_name].schemas
-        else:
-            raise ValueError(f"Invalid object type: {self}")
+        # core_object = self.get_core_object()
+        # if isinstance(core_object, Schema):
+        #     return root.databases[self.database_name].schemas
+        # else:
+        #     raise ValueError(f"Invalid object type: {self}")
+        return root.databases[self.database_name].schemas
 
 
-SnowflakeClasses = [SnowflakeDatabase, SnowflakeSchema]
+class SnowflakeTableColumn(BaseModel):
+    name: str = Field(description="The name of the column")
+    datatype: str = Field(description="The data type of the column")
 
 
-# TODOS:
-# - Add additional assertions at the class level if possible
+class SnowflakeTable(ObjectMetadata):
+    database_name: str = Field(description="The database the table belongs to")
+    schema_name: str = Field(description="The schema the table belongs to")
+    kind: Literal["PERMANENT", "TRANSIENT"] = Field(
+        default="PERMANENT", description="The kind of table"
+    )
+    # Columns only used if creating a table
+    columns: list[SnowflakeTableColumn] = Field(
+        default=None, description="The columns of the table"
+    )
+
+    def get_core_object(self):
+        return Table.from_dict(self.__dict__)
+
+    def get_core_path(self, root: Root):
+        # core_object = self.get_core_object()
+        # if isinstance(core_object, Table):
+        #     return root.databases[self.database_name].schemas[self.schema_name].tables
+        # else:
+        #     raise ValueError(f"Invalid object type: {self}")
+        root.databases[self.database_name].schemas[self.schema_name].tables
+
+
+class SnowflakeWarehouse(ObjectMetadata):
+    warehouse_type: Literal["STANDARD", "SNOWPARK-OPTIMIZED"] = Field(
+        default="STANDARD", description="The type of warehouse"
+    )
+    warehouse_size: Literal[
+        "X-SMALL",
+        "SMALL",
+        "MEDIUM",
+        "LARGE",
+        "X-LARGE",
+        "2X-LARGE",
+        "3X-LARGE",
+        "4X-LARGE",
+    ] = Field(default=None, description="The size of the warehouse")
+    auto_suspend: int = Field(
+        default=None,
+        description="The number of minutes of inactivity before the warehouse is automatically suspended",
+    )
+    auto_resume: bool = Field(
+        default=None, description="Whether the warehouse is automatically resumed"
+    )
+    initially_suspended: bool = Field(
+        default=None, description="Whether the warehouse is initially suspended"
+    )
+
+    def get_core_object(self):
+        return Warehouse.from_dict(self.__dict__)
+
+    def get_core_path(self, root: Root):
+        # core_object = self.get_core_object()
+        # if isinstance(core_object, Warehouse):
+        #     return root.warehouses
+        # else:
+        #     raise ValueError(f"Invalid object type: {self}")
+        return root.warehouses
+
+
+SnowflakeClasses = [
+    SnowflakeDatabase,
+    SnowflakeSchema,
+    SnowflakeTable,
+    SnowflakeWarehouse,
+]
--- mcp_server_snowflake/object_manager/tools.py
+++ mcp_server_snowflake/object_manager/tools.py
@@ -4,9 +4,10 @@ from fastmcp import FastMCP
 from snowflake.core import CreateMode, Root
 
 from mcp_server_snowflake.object_manager.objects import ObjectMetadata, SnowflakeClasses
+from mcp_server_snowflake.utils import SnowflakeException
 
 
-async def create_object(
+def create_object(
     object_type: ObjectMetadata,
     root: Root,
     mode: Literal["error_if_exists", "replace", "if_not_exists"] = "error_if_exists",
@@ -21,13 +22,36 @@ async def create_object(
         create_mode = CreateMode.if_not_exists
     core_object = object_type.get_core_object()
     core_path = object_type.get_core_path(root=root)
-    return core_path.create(core_object, mode=create_mode)
+    try:
+        return core_path.create(core_object, mode=create_mode)
+    except Exception as e:
+        raise SnowflakeException(tool="create_object", message=e)
 
 
 def drop_object(object_type: ObjectMetadata, root: Root, if_exists: bool = False):
     core_object = object_type.get_core_object()
     core_path = object_type.get_core_path(root=root)
-    return core_path[core_object.name].drop(if_exists=if_exists)
+    try:
+        return core_path[core_object.name].drop(if_exists=if_exists)
+    except Exception as e:
+        raise SnowflakeException(tool="drop_object", message=e)
+
+
+def describe_object(object_type: ObjectMetadata, root: Root):
+    core_object = object_type.get_core_object()
+    core_path = object_type.get_core_path(root=root)
+    try:
+        return core_path[core_object.name].fetch().to_dict()
+    except Exception as e:
+        raise SnowflakeException(tool="describe_object", message=e)
+
+
+def list_objects(object_type: ObjectMetadata, root: Root, like: str = None):
+    core_path = object_type.get_core_path(root=root)
+    try:
+        return core_path.iter(like=like, limit=100)
+    except Exception as e:
+        raise SnowflakeException(tool="list_objects", message=e)
 
 
 def initialize_object_manager_tools(server: FastMCP, root: Root):
@@ -50,3 +74,16 @@ def initialize_object_manager_tools(server: FastMCP, root: Root):
             if_exists: bool = False,
         ):
             return drop_object(object_type, root, if_exists)
+
+        @server.tool(name=f"describe_{object_name}")
+        def describe_object_tool(
+            object_type: object_type,  # type: ignore
+        ):
+            return describe_object(object_type, root)
+
+        @server.tool(name=f"list_{object_name}s")
+        def list_objects_tool(
+            object_type: object_type,  # type: ignore
+            like: str | None = None,
+        ):
+            return list_objects(object_type, root, like)
